42bf577c3e84aecb34c6c0f79bcaff445abbf3db
qo0 <15694466+rollraw@users.noreply.github.com>
2026-03-24 23:01:13 +0200

    ✨ feat(tests/dtoa): initialize converter of `double_conversion` implementation in constructor

--- tests/value-string/double_conversion.h
+++ tests/value-string/double_conversion.h
@@ -6,6 +6,9 @@
 class CDoubleConversionToString : public CValueToString<double>
 {
 public:
+	CDoubleConversionToString() :
+		converter(double_conversion::DoubleToStringConverter::EMIT_POSITIVE_EXPONENT_SIGN | double_conversion::DoubleToStringConverter::EMIT_TRAILING_DECIMAL_POINT | double_conversion::DoubleToStringConverter::EMIT_TRAILING_ZERO_AFTER_POINT, "inf", "nan", 'e', 0, 0, 0, 0) { }
+
 	[[nodiscard]] const char* GetName() const override
 	{
 		return "GOOGLE";
@@ -19,7 +22,10 @@ public:
 	char* ToString(char* szBuffer, std::size_t nBufferSize, const double dlValue) override
 	{
 		double_conversion::StringBuilder stringBuilder(szBuffer, static_cast<int>(nBufferSize));
-		double_conversion::DoubleToStringConverter::EcmaScriptConverter().ToFixed(dlValue, 17, &stringBuilder);
+		converter.ToFixed(dlValue, 17, &stringBuilder); // @note: doesn't support values greater than 1e60
 		return szBuffer;
 	}
+
+private:
+	double_conversion::DoubleToStringConverter converter;
 };
